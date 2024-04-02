d42fbb867fe91e95675518655412b7c5fc5eea6f
Valentin Mehalin <valentin.mehalin06@orange.fr>
2024-04-02 16:05:10 +0200

    correction

--- 01 demo/03_invitation.sql
+++ 01 demo/03_invitation.sql
@@ -1,13 +1,17 @@
 USE invitation;
 
+-- DEFAULT
+-- si on ne met pas la contrainte de NULLITÉ
+-- alors le champ peut être NULL
+
 CREATE TABLE invPersonne( 
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    prenom VARCHAR(255) NOT NULL, 
-    nom VARCHAR(255) NOT NULL, 
+    prenom VARCHAR(50) NOT NULL, 
+    nom VARCHAR(50) NOT NULL, 
     age TINYINT(1) NOT NULL, 
-    date_inscription DATE NOT NULL, 
+    date_inscription DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
     status boolean, 
-    type ENUM ('membre','non membre'), 
+    type ENUM ('membre','non membre') NOT NULL DEFAULT 'non membre', 
     description text, 
-    salaire_annuel int 
+    salaire_annuel int NOT NULL
 ) ENGINE=InnoDB;
