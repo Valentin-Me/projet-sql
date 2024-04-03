ac3b79977e111d15e5ae81bb2f725120a22c0394
Valentin Mehalin <valentin.mehalin06@orange.fr>
2024-04-03 16:01:37 +0200

    TP03 done

--- 01 demo/TP03.sql
+++ 01 demo/TP03.sql
@@ -1,25 +1,35 @@
 -- 1 - Afficher toutes les factures avec le nom des clients
- SELECT facture.reference,facture.info,facture.total,facture.date_crea,facture.date_paiement,client.nom
+ SELECT facture.*,client.nom
  FROM `facture`
  INNER JOIN devis ON facture.devis_id = devis.id
  INNER JOIN projet ON devis.projet_id = projet.id
  INNER JOIN client ON projet.client_id = client.id;
 
 -- 2 - Afficher le nombre de factures par client afficher 0 factures si il n'y a pas de factures
- SELECT COUNT(client.nom) 
+ SELECT client.nom
+ COUNT(facture.id) 
  FROM `client` 
- INNER JOIN projet ON client.id = projet.client_id 
- INNER JOIN devis ON projet.id = devis.projet_id 
- INNER JOIN facture ON devis.id = facture.devis_id;
+ LEFT JOIN projet ON client.id = projet.client_id 
+ LEFT JOIN devis ON projet.id = devis.projet_id 
+ LEFT JOIN facture ON devis.id = facture.devis_id
+ GROUP BY (client.id) ;
 
 -- 3 - afficher le chiffre d'affaire par client
- 
+ SELECT client.nom, SUM(facture.total) 
+ FROM `client` 
+ LEFT JOIN projet ON client.id = projet.client_id 
+ LEFT JOIN devis ON projet.id = devis.projet_id 
+ LEFT JOIN facture ON devis.id = facture.devis_id 
+ GROUP BY (client.id);
 
 -- 4 - afficher le CA total
- 
+ SELECT SUM(total) FROM facture;
 
 -- 5 - afficher la somme des factures en attente de paiement
- 
+ SELECT SUM(total) FROM facture WHERE date_paiement IS NULL;
 
 -- 6 - afficher les factures en retard de paiment 30 jours max avec le nombre de jours de retard
- 
+ SELECT facture.*, DATEDIFF(NOW(), date_crea) 
+ FROM facture 
+ WHERE DATEDIFF(NOW(), date_crea)>30 
+ AND date_paiement IS NULL;
